8e4667681fece4a41530fbd3cd77a822a80ebc6b
YoshiAki <yoshiakiley@gmail.com>
2023-07-14 15:23:18 +0800

    update

--- katana.go
+++ katana.go
@@ -2,6 +2,7 @@ package katana
 
 import (
 	"context"
+	"fmt"
 	"github.com/yoshiakiley/katana/core"
 	"github.com/yoshiakiley/katana/service"
 	"github.com/yoshiakiley/katana/store"
@@ -13,11 +14,18 @@ type StoreType string
 const StoreTypeMongo StoreType = "mongo"
 
 func NewMongoService[R core.IObject](schema, collection string) *service.Service[R] {
-	svc := &service.Service[R]{
+	svc, exists := service.Get[R](fmt.Sprintf("%s-%s", schema, collection))
+	if exists {
+		return svc
+	}
+
+	svc = &service.Service[R]{
 		Schema:     schema,
 		Collection: collection,
 	}
 	svc.Set(store.NewStore[R](mongo.NewMongoCli[R](), nil))
+
+	service.Set(fmt.Sprintf("%s-%s", schema, collection), svc)
 
 	return svc
 }
